c0d6725113433abd5de70af7f971b464a8fbe512
Navdeep JAin <navdeep.jain@jetblue.com>
2015-05-18 16:47:40 +0530

    t11

--- jenkinsTestAppTests/jenkinsTestAppTests.m
+++ jenkinsTestAppTests/jenkinsTestAppTests.m
@@ -25,16 +25,14 @@
     [super tearDown];
 }
 
-- (void)testExample {
+- (void)testExample1 {
     // This is an example of a functional test case.
     XCTAssert(YES, @"Pass");
 }
 
-- (void)testPerformanceExample {
+- (void)testExample2 {
     // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+    XCTAssertEqual(@"a", @"b", @"fail");
 }
 
 @end
